20b786749a82b52c060bee5bf6650825103cc563
limyoursun <limyoursun@gmail.com>
2023-02-20 17:30:05 +0900

    02/20 전달 파일

--- dist/js/ui_common.js
+++ dist/js/ui_common.js
@@ -7,40 +7,99 @@ $(function () {
     changeMonth: true,
     changeYear: true,
     numberOfMonths: 1,
-    regional : ["ko"],
-    dateFormat : "yy-mm-dd"
+    regional: ["ko"],
+    dateFormat: "yy-mm-dd",
   });
-  $( "[role='datepic']" ).datepicker({
-      buttonText: "날짜를 선택해주세요."
-  });  
-  var from = $( "[role='from']" ).datepicker({
+  $("[role='datepic']").datepicker({
+    buttonText: "날짜를 선택해주세요.",
+  });
+  var from = $("[role='from']").datepicker({
     buttonText: "시작날짜를 선택해주세요.",
-    onClose: function( selectedDate ) {
-      var getName=$(this).attr('name');
-      $("input[name='"+ getName +"'].to").datepicker( "option", "minDate", selectedDate );
-    }  
+    onClose: function (selectedDate) {
+      var getName = $(this).attr("name");
+      $("input[name='" + getName + "'].to").datepicker(
+        "option",
+        "minDate",
+        selectedDate
+      );
+    },
   });
-  var to = $( "[role='to']" ).datepicker({
+  var to = $("[role='to']").datepicker({
     buttonText: "종료날짜를 선택해주세요.",
-    onClose: function( selectedDate ) {
-      var getName=$(this).attr('name');
-      $("input[name='"+ getName +"'].from").datepicker( "option", "maxDate", selectedDate );
-    }
-  }); 
+    onClose: function (selectedDate) {
+      var getName = $(this).attr("name");
+      $("input[name='" + getName + "'].from").datepicker(
+        "option",
+        "maxDate",
+        selectedDate
+      );
+    },
+  });
 
   // pop
-  var popBtn = $('[openpop]');
-  popBtn.on('click', function () {
-    var target = $(this).attr('openpop');
-    $('#' + target).show();
+  var popBtn = $("[openpop]");
+  popBtn.on("click", function () {
+    var target = $(this).attr("openpop");
+    $("#" + target).show();
+  });
+  var closePop = $(".btn_pop_close");
+  closePop.on("click", function () {
+    $(this).parents(".pop_overlay").hide();
+  });
+
+  $(".con_list .more").on("click", function () {
+    $(this).toggleClass("on");
+    $(this).parent("p").nextAll("ul").slideToggle("fast");
+  });
+
+  // mobile gnb
+  $(".allMenuOpen").on("click", function (e) {
+    e.preventDefault();
+    $(this).toggleClass('close');
+    $("#allMenuBox").toggle(0).attr("tabindex", 0);
+    $("#allMenuBox").toggleClass("active");
+  });
+
+  $(".pc_con .gnb > li").on("mouseover", function (e) {
+    e.preventDefault();
+    $(this).children(".sub").addClass("active");
+  });
+  $(".pc_con .gnb > li").on("mouseleave", function (e) {
+    e.preventDefault();
+    $(this).children(".sub").removeClass("active");
+  });
+  $("#allMenuBox").removeClass("active").attr("tabindex", -1);
+
+  $("#allMenuBox > ul > li").each(function () {
+    $(this)
+      .children("a")
+      .on("click", function (e) {
+        if ($(this).siblings().length > 0) {
+          e.preventDefault();
+          $(this).parent().addClass("active").siblings().removeClass("active");
+        }
+      });
+  });
+  $("#allMenuBox > ul > li > a").each(function () {
+    if (!$(this).next().length) {
+      $(this).addClass("empty");
+      console.log("1");
+    }
+  });
+  $(".m_con > li").each(function () {
+    $(this)
+      .children("a")
+      .on("click", function (e) {
+        if ($(this).siblings().length > 0) {
+          e.preventDefault();
+          $(this).parent().addClass("active").siblings().removeClass("active");
+        }
+      });
+  });
+  $(".m_con > li > a").each(function () {
+    if (!$(this).next().length) {
+      $(this).addClass("empty");
+      console.log("3");
+    }
   })
-  var closePop = $('.btn_pop_close');
-  closePop.on('click', function () {
-    $(this).parents('.pop_overlay').hide();
-  })
-  
-  $('.con_list .more').on('click', function () {
-    $(this).toggleClass('on');
-    $(this).parent('p').nextAll('ul').slideToggle('fast');
-  })
-})
+});
